1dfeb96b45accba09f20f7081d2e09c99b9ab4d4
Jens Rydholm <spacejens@gmail.com>
2018-12-12 22:15:58 +0100

    Test case verifies that Spring injection of components works.

--- src/test/java/se/spacejens/samplewebapp/SamplewebappApplicationTests.java
+++ src/test/java/se/spacejens/samplewebapp/SamplewebappApplicationTests.java
@@ -1,10 +1,16 @@
 package se.spacejens.samplewebapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SamplewebappApplicationTests {
+
+	@Autowired
+	HelloWorldController helloWorldController;
 
 	@Test
 	void contextLoads() {
@@ -12,5 +18,10 @@ class SamplewebappApplicationTests {
 		 * The purpose of this test case is to verify that the context loads successfully (i.e. no exception is thrown).
 		 * Therefore, no additional code is needed in this test case.
 		 */
+	}
+
+	@Test
+	void componentInjectionWorks() {
+		assertNotNull(helloWorldController);
 	}
 }
